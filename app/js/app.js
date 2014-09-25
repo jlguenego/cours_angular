dc5bc5bcfe24d1ace71690869401dc445e1f5b69
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-09-25 16:53:52 +0200

    No multi level chapter

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,33 @@
+function setSidebarHeight() {
+	var winHeight = $(window).height();
+	var height = winHeight - 88;
+
+	var winBottom = $(window).scrollTop() + winHeight;
+
+	var footerPos = $('footer').offset().top;
+	if (winBottom > footerPos) {
+		// The footer is on the screen
+		height -= winBottom - footerPos
+	}
+
+	$('.my-sidebar').height(height);
+}
+
 (function() {
 	var app = angular.module('angular_cours_app', [ 'my_layout' ]);
 
-	app.controller('MyAppController', ['$scope', function($scope) {
+	app.controller('MyAppController', ['$scope','$http', function($scope, $http) {
 		$scope.now = new Date();
+
+		$http.get('data/map.json')
+			.success(function(data) {
+				$scope.map = data;
+			})
+			.error(function() {
+				alert('Cannot find map...');
+			});
 	}]);
+	$(window).resize(setSidebarHeight);
+
+	$(window).scroll(setSidebarHeight);
 })();
